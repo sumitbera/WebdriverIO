4facd81432ee0d150c8cdb3e6a6d516818fc4755
sumitbera <sumit.bera@live.in>
2022-10-26 14:00:29 +1100

    Moved home screen function to separate page module

---
+++ test/pages/home.page.js
@@ -0,0 +1,20 @@
+const Page = require("../pages/page");
+const HomeScreen = require('../screens/home')
+const CommonTest = require('../../utility/CommonTest')
+
+class HomePage extends Page {
+
+    async open() {
+        await super.open('/')
+    }
+
+    async clickOnElementSection() {
+        await CommonTest.doClick(HomeScreen.elementSection)
+    }
+
+    async scrollToElementSection() {
+        await CommonTest.doScrollTillText(HomeScreen.elementSection);
+    }
+
+}
+module.exports = new HomePage();
