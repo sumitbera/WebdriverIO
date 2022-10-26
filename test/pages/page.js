09ae4504d1fbefb3a86fa8b9ebac461c8ca9eca7
sumitbera <sumit.bera@live.in>
2022-10-26 14:01:02 +1100

    Moved to page parent function to separate module

--- test/pages/page.js
+++ test/pages/page.js
@@ -1,16 +1,3 @@
-/**
-* main page object containing all methods, selectors and functionality
-* that is shared across all page objects
-*/
-// module.exports = class Page {
-//     /**
-//     * Opens a sub page of the page
-//     * @param path path of the sub page (e.g. /path/to/page.html)
-//     */
-//     open (path) {
-//         return browser.url(path)
-//     }
-// }
 module.exports = class Page{
     constructor(){
         this.title = 'My Page'
