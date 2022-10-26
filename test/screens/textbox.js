1c0d89c811ed59882c6f6da4dfb8328fd6fa0139
sumitbera <sumit.bera@live.in>
2022-10-26 14:46:50 +1100

    Moved functions to screens for textbox

--- test/screens/textbox.js
+++ test/screens/textbox.js
@@ -26,34 +26,6 @@ class TextBoxScreen extends Page{
     get submitBtn() {
         return $(textBoxLocator.SUBMIT_BTN);
     }
-
-    async clickTextBoxOption(){
-        await this.textBoxOption.click();
-    }
-
-    async enterFullName(){
-        await this.fullName.addValue('Sumit Bera');
-    }
-
-    async enterEmailAddress(){
-        await this.email.addValue('sumit_test@xyz.com');
-    }
-
-    async enterCurrentAddress(){
-        await this.currentAddress.addValue('26 Coowarra Way, Berwik');
-    }
-
-    async enterPermanentAddress(){
-        await this.permanentAddress.addValue('26 Coowarra Way, Berwik');
-    }
-
-    async clickSubmitBtn(){
-        await this.submitBtn.click();
-    }
-
-    async scrollTillSubmitBtn(){
-        await this.submitBtn.scrollIntoView();
-    }
 }
 
 module.exports = new TextBoxScreen();
