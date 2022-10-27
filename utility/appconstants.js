562f265f78a0b640b55a74106a85fb7e1369f910
sumitbera <sumit.bera@live.in>
2022-10-27 14:12:35 +1100

    Added browser key value to constant

--- utility/appconstants.js
+++ utility/appconstants.js
@@ -1,5 +1,6 @@
 module.exports = {
     FULL_NAME: 'Sumit Bera',
     EMAIL_ADDRESS: 'sumit_test@xyz.com',
-    ADDRESS: '26 Coowarra Way, Berwik'
+    ADDRESS: '26 Coowarra Way, Berwik',
+    LINK_TEXT: 'demo'
 }
