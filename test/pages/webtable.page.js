c4851ccf5b3d26d9d6a78de5073e36559472fd3d
sumitbera <sumit.bera@live.in>
2022-10-31 22:09:05 +1100

    Added registration logic

--- test/pages/webtable.page.js
+++ test/pages/webtable.page.js
@@ -1,6 +1,8 @@
 const Page = require("../pages/page");
 const webTable = require("../../locators/webtable");
 const CommonTest = require("../../utility/commontest");
+const Constants = require('../../utility/appconstants');
+const dataGenerator = require('../../utility/datagenerator');
 
 class WebTablePage extends Page {
   get webTableOption() {
@@ -11,9 +13,80 @@ class WebTablePage extends Page {
     return $(webTable.ADD_BTN);
   }
 
+  get firstName(){
+    return $(webTable.FIRST_NAME);
+  }
+
+  get lastName(){
+    return $(webTable.LAST_NAME);
+  }
+
+  get emailAddress(){
+    return $(webTable.EMAIL);
+  }
+
+  get age(){
+    return $(webTable.AGE);
+  }
+
+  get salary(){
+    return $(webTable.SALARY);
+  }
+
+  get department(){
+    return $(webTable.DEPARTMENT);
+  }
+
+  get submitBtn(){
+    return $(webTable.SUBMIT_BTN);
+  }
+
   async clickWebTableOption() {
     await expect(this.webTableOption).toBeEnabled();
     await CommonTest.doClick(this.webTableOption);
+  }
+
+  async clickAddBtn(){
+    await expect(this.addBtn).toBeEnabled();
+    await CommonTest.doClick(this.addBtn);
+  }
+
+  async enterFirstNameForRegistration(){
+    await expect(this.firstName).toBeDisplayed();
+    await CommonTest.doAddValue(this.firstName, Constants.FIRST_NAME);
+  }
+
+  async enterLastNameForRegistration(){
+    await expect(this.lastName).toBeDisplayed();
+    await CommonTest.doAddValue(this.lastName, Constants.LAST_NAME);
+  }
+
+  async enterYourEmailAddress(){
+    await expect(this.emailAddress).toBeDisplayed();
+    let email = dataGenerator.getRandomEmail("@xyz.com",10);
+    console.log(email)
+    await CommonTest.doAddValue(this.emailAddress, email);
+  }
+
+  async enterYourAge(){
+    await expect(this.age).toBeDisplayed();
+    await CommonTest.doAddValue(this.age, Constants.AGE);
+  }
+
+  async enterYourSalary(){
+    await expect(this.salary).toBeDisplayed();
+    await CommonTest.doAddValue(this.salary, Constants.SALARY);
+  }
+
+  async enterYourDepartment(){
+    await expect(this.department).toBeDisplayed();
+    await CommonTest.doAddValue(this.department, Constants.DEPARTMENT);
+  }
+
+  async clickSubmitBtn(){
+    await expect(this.submitBtn).toBeDisplayed();
+    await expect(this.submitBtn).toBeEnabled();
+    await CommonTest.doClick(this.submitBtn);
   }
 
   async getListOfItemsFromTable() {
