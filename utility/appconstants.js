c887902b60b550bd3dd2cd6d844af9813acbfe79
sumitbera <sumit.bera@live.in>
2022-10-31 22:09:47 +1100

    Added test data for user registration

--- utility/appconstants.js
+++ utility/appconstants.js
@@ -2,5 +2,10 @@ module.exports = {
     FULL_NAME: 'Sumit Bera',
     EMAIL_ADDRESS: 'sumit_test@xyz.com',
     ADDRESS: '26 Coowarra Way, Berwik',
-    LINK_TEXT: 'demo'
+    LINK_TEXT: 'demo',
+    FIRST_NAME: 'Sumit',
+    LAST_NAME: 'Bera',
+    AGE: 32,
+    DEPARTMENT: 'Engineering',
+    SALARY: '150000'
 }
