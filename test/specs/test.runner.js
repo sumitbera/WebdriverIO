765f31a15e08f0de8ce193a72cd5b05affe813f1
sumitbera <sumit.bera@live.in>
2022-10-31 17:14:00 +1100

    Added test for fetching table data

--- test/specs/test.runner.js
+++ test/specs/test.runner.js
@@ -1,35 +1,42 @@
-const homePage = require('../pages/home.page');
-const textBoxPage = require('../pages/textbox.page');
-const checkBoxPage = require('../pages/checkbox.page');
+const homePage = require("../pages/home.page");
+const textBoxPage = require("../pages/textbox.page");
+const checkBoxPage = require("../pages/checkbox.page");
+const webTablePage = require("../pages/webtable.page");
 
 describe("interaction with web element", () => {
+  before("Launch ToolsQA application", async () => {
+    await homePage.open();
+    await browser.pause(1000);
+  });
 
-    before('Launch ToolsQA application', async () => {
-        await homePage.open()
-        await browser.pause(1000)
-    })
+  it("Click Element Section", async () => {
+    await homePage.scrollToElementSection();
+    await homePage.clickOnElementSection();
+    await browser.pause(1000);
+  });
 
-    it("Search Elements", async () => {
-        await homePage.scrollToElementSection()
-        await homePage.clickOnElementSection()
-        await browser.pause(1000)
-    })
+  it("Enter value in Text box", async () => {
+    await browser.pause(1000);
+    await textBoxPage.clickTextBoxOption();
+    await textBoxPage.enterFullName();
+    await textBoxPage.enterEmailAddress();
+    await textBoxPage.enterCurrentAddress();
+    await textBoxPage.enterPermanentAddress();
+    await textBoxPage.scrollTillSubmitBtn();
+    await textBoxPage.clickSubmitBtn();
+  });
 
-    it("Enter value in Text box", async () => {
-        await browser.pause(1000)
-        await textBoxPage.clickTextBoxOption();
-        await textBoxPage.enterFullName();
-        await textBoxPage.enterEmailAddress();
-        await textBoxPage.enterCurrentAddress();
-        await textBoxPage.enterPermanentAddress();
-        await textBoxPage.scrollTillSubmitBtn();
-        await textBoxPage.clickSubmitBtn();
-    })
+  it("Select checkbox options", async () => {
+    await browser.pause(1000);
+    await checkBoxPage.clickCheckBoxOption();
+    await checkBoxPage.clickHomeCheckOption();
+    await checkBoxPage.clickToggleOption();
+  });
 
-    it("Select checkbox options", async () => {
-        await browser.pause(1000);
-        await checkBoxPage.clickCheckBoxOption();
-        await checkBoxPage.clickHomeCheckOption();
-        await checkBoxPage.clickToggleOption();
-    })
+  it("Fetch data from web table", async () => {
+    await browser.pause(1000);
+    await webTablePage.clickWebTableOption();
+    await webTablePage.getListOfItemsFromTable();
+    await browser.pause(5000);
+  });
 });
