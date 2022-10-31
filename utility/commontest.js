ea52e5c4e627a2821946705bbf25ecddf189d93e
sumitbera <sumit.bera@live.in>
2022-10-31 22:10:15 +1100

    removed unused spacing

--- utility/commontest.js
+++ utility/commontest.js
@@ -1,29 +1,27 @@
 class CommonTest {
+  async doClick(element) {
+    await element.waitForDisplayed();
+    await element.click();
+  }
 
-    async doClick(element) {
-        await element.waitForDisplayed()
-        await element.click()
-    }
+  async doAddValue(element, value) {
+    await element.waitForDisplayed({ timeout: 3000 });
+    await element.addValue(value);
+  }
 
-    async doAddValue(element, value) {
-        await element.waitForDisplayed({ timeout: 3000 });
-        await element.addValue(value)
-    }
+  async doGetText(element) {
+    await element.waitForDisplayed({ timeout: 3000 });
+    return element.getText();
+  }
 
-    async doGetText(element) {
-        await element.waitForDisplayed({ timeout: 3000 });
-        return element.getText();
-    }
+  async doScrollTillText(element) {
+    await element.waitForDisplayed({ timeout: 3000 });
+    await element.scrollIntoView();
+  }
 
-    async doScrollTillText(element) {
-        await element.waitForDisplayed({ timeout: 3000 });
-        await element.scrollIntoView();
-    }
-
-    async doVerifyElementIsVisible(element) {
-        await element.waitForDisplayed({ timeout: 3000 });
-        console.log("Element visible:" + element)
-    }
+  async doVerifyElementIsVisible(element) {
+    await element.waitForDisplayed({ timeout: 3000 });
+    console.log("Element visible:" + element);
+  }
 }
-
-module.exports = new CommonTest()
+module.exports = new CommonTest();
