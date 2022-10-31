d00db1183574efdeb01ccab670e278cc9f38dd34
sumitbera <sumit.bera@live.in>
2022-10-31 22:08:24 +1100

    Added function to generate random email address

---
+++ utility/datagenerator.js
@@ -0,0 +1,11 @@
+function getRandomEmail(domain, length) {
+    var text = "";
+    var possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+    for (var i = 0; i < length; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return text + domain;
+  }
+  module.exports = {getRandomEmail}
