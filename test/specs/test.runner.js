0a0fafcfb9c621aa6b14578fa13c492ac8bbf257
sumitbera <sumit.bera@live.in>
2023-04-15 21:26:42 +1000

    Updated test for launching qr site

--- test/specs/test.runner.js
+++ test/specs/test.runner.js
@@ -1,54 +1,12 @@
-const homePage = require("../pages/home.page");
-const textBoxPage = require("../pages/textbox.page");
-const checkBoxPage = require("../pages/checkbox.page");
-const webTablePage = require("../pages/webtable.page");
+const qrPage = require("../pages/qr.page");
 
-describe("interaction with web element", () => {
-  before("Launch ToolsQA application", async () => {
-    await homePage.open();
+describe("Scan and Fetch QR Code", () => {
+  before("Launch QR application", async () => {
+    await qrPage.open();
     await browser.pause(1000);
   });
 
-  it("Click Element Section", async () => {
-    await homePage.scrollToElementSection();
-    await homePage.clickOnElementSection();
-    await browser.pause(1000);
-  });
+  it("Scan QR Code",async () =>{
 
-  it("Enter value in Text box", async () => {
-    await browser.pause(1000);
-    await textBoxPage.clickTextBoxOption();
-    await textBoxPage.enterFullName();
-    await textBoxPage.enterEmailAddress();
-    await textBoxPage.enterCurrentAddress();
-    await textBoxPage.enterPermanentAddress();
-    await textBoxPage.scrollTillSubmitBtn();
-    await textBoxPage.clickSubmitBtn();
-  });
-
-  it("Select checkbox options", async () => {
-    await browser.pause(1000);
-    await checkBoxPage.clickCheckBoxOption();
-    await checkBoxPage.clickHomeCheckOption();
-    await checkBoxPage.clickToggleOption();
-  });
-
-  it("Fetch data from web table", async () => {
-    await browser.pause(1000);
-    await webTablePage.clickWebTableOption();
-    await webTablePage.getListOfItemsFromTable();
-  });
-
-  it("Enter registration details", async () => {
-    await browser.pause(1000);
-    await webTablePage.clickAddBtn();
-    await webTablePage.enterFirstNameForRegistration();
-    await webTablePage.enterLastNameForRegistration();
-    await webTablePage.enterYourEmailAddress();
-    await webTablePage.enterYourAge();
-    await webTablePage.enterYourSalary();
-    await webTablePage.enterYourDepartment();
-    await webTablePage.clickSubmitBtn();
-    await browser.pause(5000);
-  });
+  })
 });
