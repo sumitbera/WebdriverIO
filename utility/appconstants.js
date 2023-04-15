4ddf1fe1424640ce9f536f69e5e53e7ca5046da3
sumitbera <sumit.bera@live.in>
2023-04-15 21:26:52 +1000

    Updated link text value

--- utility/appconstants.js
+++ utility/appconstants.js
@@ -1,11 +1,3 @@
 module.exports = {
-    FULL_NAME: 'Sumit Bera',
-    EMAIL_ADDRESS: 'sumit_test@xyz.com',
-    ADDRESS: '26 Coowarra Way, Berwik',
-    LINK_TEXT: 'demo',
-    FIRST_NAME: 'Sumit',
-    LAST_NAME: 'Bera',
-    AGE: 32,
-    DEPARTMENT: 'Engineering',
-    SALARY: '150000'
+    LINK_TEXT: 'https://test-studio.oliu.id/login',
 }
