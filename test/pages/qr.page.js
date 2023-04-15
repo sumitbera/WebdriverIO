27bc152f067fca5f294d9a8dcfb45af7f6b5f877
sumitbera <sumit.bera@live.in>
2023-04-15 22:35:18 +1000

    Added functions to read and capture screenshot

--- test/pages/qr.page.js
+++ test/pages/qr.page.js
@@ -1,21 +1,42 @@
 const Page = require("../pages/page");
 const qrLocator = require("../../locators/qrscreen");
 const CommonTest = require("../../utility/commontest");
-const Constants = require('../../utility/appconstants');
+const Constants = require("../../utility/appconstants");
+const fs = require("fs");
 
 class QrPage extends Page {
-  
-    async open() {
-        super.open('/')
-        await expect(browser).toHaveUrlContaining(Constants.LINK_TEXT)
-    }
+  async open() {
+    super.open("/");
+    expect(browser).toHaveUrlContaining(Constants.LINK_TEXT);
+  }
 
-    get qrLayout() {
+  get qrLayout() {
     return $(qrLocator.QR_CODE_LAYOUT);
   }
 
   get qrImage() {
     return $(qrLocator.QR_IMAGE);
   }
+
+  async verifyQrLayout() {
+    await expect(this.qrLayout).toBeDisplayed();
+  }
+
+  async verifyQrImage() {
+    await expect(this.qrImage).toBeDisplayedInViewport();
+  }
+
+  async captureQrCode() {
+    const qrImage = await $(qrLocator.QR_IMAGE);
+    await qrImage.saveScreenshot(Constants.QR_FOLDER);
+  }
+  async deleteFileIfExists() {
+    try {
+      fs.unlinkSync(Constants.QR_FOLDER);
+      console.log("QR Deleted successfully...", Constants.QR_FOLDER);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
-module.exports = new QrPage()
+module.exports = new QrPage();
